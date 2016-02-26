1286e42e40d6bafe8e0e88a1f995a248ce14a3ce
Angel Alvarado <angel.angelio@gmail.com>
2016-02-25 16:19:50 -0800

    Adding configuration to setup simplesamlphp

---
+++ cnf/local.settings.php
@@ -0,0 +1,11 @@
+<?php
+$db_url='mysql://drupal7:drupal7@localhost/drupal7';
+$databases=array('default' => array(
+    'default' => array(
+        'database' => 'drupal7',
+        'username' => 'drupal7',
+        'password' => 'drupal7',
+        'driver' => 'mysql',
+        'host' => 'localhost',
+    ),
+),);
