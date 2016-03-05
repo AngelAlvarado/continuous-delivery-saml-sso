41dccc52ba2bd943b508b1f2d7a937159671ee78
Angel Alvarado <angel.angelio@gmail.com>
2016-03-05 02:03:15 -0800

    Configuring SP and IdP, setting up certificates and metadata, enabling UserPass build-in module

--- build/install.sh
+++ build/install.sh
@@ -3,10 +3,12 @@
 set -e #exit on error
 path=$(dirname "$0") # .
 base=$(cd $path/.. && pwd) #keep track of root directory
-if [ ! -d "$path/www" ]; then
+echo "$path/www"
+if [ ! -d "$base/www" ]; then
   wget https://ftp.drupal.org/files/projects/drupal-7.43.zip -P $base
   unzip drupal-7.43.zip -d $base
   mv $base/drupal-7.43 $base/www
+  rm -rf $base/drupal-7.43*
 fi
 drush="drush $drush_flags -y -r $base/www" #prepare drush command to receive argument and always accept(Y)
 chmod -R +w $base/www/sites/default #Make sure  Drupal does not messes up with our permissions
@@ -15,13 +17,20 @@ echo "Symlink settings.php into our Drupal.  "
 ln -sf $base/cnf/settings.php $base/www/sites/default/ #from host to guest (vagrant/* folders are already sync by VirtualBox)
 echo "Installing Drupal like a boss."
 $drush si --site-name=no-excuses --account-pass=admin
-if [ ! -d "$path/simplesamlphp" ]; then
+if [ ! -d "$base/simplesamlphp" ]; then
   echo "Downloading simplesamlphp."
   wget https://simplesamlphp.org/res/downloads/simplesamlphp-1.13.0.tar.gz -P $base
   tar -zxvf $base/simplesamlphp-1.13.0.tar.gz -C $base
   mv $base/simplesamlphp-1.13.0 $base/simplesamlphp
+  rm -rf $base/simplesamlphp-1.13.0.tar*
 fi
 echo "Configuring simpleSAMLphp."
-cp $base/cnf/config.php $base/simplesamlphp/config/ #Use our settings
+cp $base/cnf/SAML/config.php $base/simplesamlphp/config/ #Use our settings
+cp $base/cnf/SAML/authsources.php $base/simplesamlphp/config/ #Use basic configuration
+cp $base/cnf/SAML/saml20-idp-hosted.php $base/simplesamlphp/metadata/ #Set IdP hosted info
+cp $base/cnf/SAML/saml20-idp-remote.php $base/simplesamlphp/metadata/ #Set IdP remote info
+cp $base/cnf/SAML/saml20-sp-remote.php $base/simplesamlphp/metadata/ #Set SP remote info
+cp -R $base/cnf/SAML/cert $base/simplesamlphp/cert #Copying my own certs; Warning this can't be used in a Prod instance
+touch $base/simplesamlphp/modules/exampleauth/enable #Enable UserPass buil-in module
 sudo ln -sf $base/simplesamlphp /var/simplesaml #from host to guest (vagrant/* folders are already sync by VirtualBox)
 echo "Done."
--- readme.md
+++ readme.md
@@ -2,9 +2,9 @@
 
 For details of creating this Vagrant box take a look here: http://www.molanco.com/vagrant-box-drupal-7-simplesamlphp
 
-### Prerequisites
+### Optional
 
-Once you download this repo. Download    ```Drupal 7```,     ```SimpleSAMLphp 1.3``` and the     ```simplesamlphp_auth 7.x-3.x``` module place them into     ```www```,     ```simplesamlphp``` and     ```www/sites/all/modules``` respectivelly. 
+Clone this repo. Then Download    ```Drupal 7```,     ```SimpleSAMLphp 1.3```  and place them into     ```www```,     ```simplesamlphp```. You can then use   ```simplesamlphp_auth 7.x-3.x``` or  ```multiple_idp_simplesamlphp```
 
 ### Box details
 
@@ -20,8 +20,11 @@ Drupal IP: 192.168.33.99
 SimpleSAMLphp: 192.168.33.99/simplesaml
 Hostname: drupal7
 Document root: /vagrant/www
-Samlphp library: /vagrant/simplesaml 
-Mariadb password, user, database: drupal7:drupal7:drupal7
-Drupal user, password: admin:admin
-SimpleSAMLphp user, password: admin:1234
+Samlphp library: /vagrant/simplesaml
+Mariadb password, user, database; drupal7:drupal7:drupal7
+Drupal user, password; admin:admin
+SimpleSAMLphp user, password; admin:1234
+SimpleSAMLphp molanco-authentication has two users; admin1:admin1 and admin2:admin2
+SimpleSAMLphp IdP name: openidp
 ```
+Warning: Don't use this box in a Production Environment.
